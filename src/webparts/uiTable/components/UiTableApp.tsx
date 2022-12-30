3bcbea2cc4841117b087473b349619cd1d326692
timellidge <76814895+timellidge@users.noreply.github.com>
2022-12-30 18:09:26 +0000

    accessing 2 lists

--- src/webparts/uiTable/components/UiTableApp.tsx
+++ src/webparts/uiTable/components/UiTableApp.tsx
@@ -4,15 +4,28 @@ import { IUiTableProps } from './IUiTableProps';
 import UiTable from './UiTable'
 import { SPHttpClient, SPHttpClientResponse } from '@microsoft/sp-http'
 
-export default class UiTableApp extends React.Component<IUiTableProps, {}> {
+export default class UiTableApp extends React.Component<IUiTableProps, any> {
+
+    state = {
+        items:[],
+        moreitems:[]
+    };
 
     private _client: SPHttpClient = this.props.ctx.spHttpClient;
 
     private _getTableData() {
         let url = "https://justsharepoint.sharepoint.com/sites/JSPProjects/_api/web/lists/GetByTitle('DemoList')/items";
         this._getSPData(this._client, url).then(d => {
-            let data = d;
-            console.log(JSON.stringify(data));
+            let data = d.value;
+            this.setState({ items: data });
+        });
+    }
+
+    private _getTableData2() {
+        let url = "https://justsharepoint.sharepoint.com/sites/JSPProjects/_api/web/lists/GetByTitle('Notifications')/items";
+        this._getSPData(this._client, url).then(d => {
+            let data = d.value;
+            this.setState({ moreitems: data });
         });
     }
 
@@ -24,14 +37,20 @@ export default class UiTableApp extends React.Component<IUiTableProps, {}> {
 
     componentDidMount() {
         this._getTableData();
+        this._getTableData2();
     }
 
     public render(): React.ReactElement<IUiTableProps> {
         return (
             <section className={`${styles.uiTable} `}>
+                <h2>Hi about to go deeper </h2>
                 <div className={styles.welcome}>
                     <UiTable />
                 </div>
+                <h1>LIST 1 DATA</h1>
+                <div>{JSON.stringify(this.state.items)}</div>
+                <h1>LIST 2 DATA</h1>
+                <div>{JSON.stringify(this.state.moreitems)}</div>
             </section>
         );
     }
